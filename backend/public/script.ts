8e80cd6620c55a4262d8c8226590baf36f7cb28d
Hairum Qureshi <hqureshi@udel.edu>
2025-10-16 16:43:55 -0400

    added variables, logic to show/hide the AI time display, added loading message, and added markdown to HTML conversion function

--- backend/public/script.ts
+++ backend/public/script.ts
@@ -1,7 +1,39 @@
+let totalSeconds = 0;
+
+function setCounter() {
+	totalSeconds += 1;
+}
+
+function markdownToHTML(html: string) {
+	html = html.replace(/^###### (.*$)/gim, "<h6>$1</h6>");
+	html = html.replace(/^##### (.*$)/gim, "<h5>$1</h5>");
+	html = html.replace(/^#### (.*$)/gim, "<h4>$1</h4>");
+	html = html.replace(/^### (.*$)/gim, "<h3>$1</h3>");
+	html = html.replace(/^## (.*$)/gim, "<h2>$1</h2>");
+	html = html.replace(/^# (.*$)/gim, "<h1>$1</h1>");
+
+	html = html.replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>");
+	html = html.replace(/\*(.*?)\*/gim, "<em>$1</em>");
+	html = html.replace(/`(.*?)`/gim, "<code>$1</code>");
+	html = html.replace(/^> (.*$)/gim, "<blockquote>$1</blockquote>");
+	html = html.replace(/\n/gim, "<br />");
+
+	return html;
+}
+
+const aiResponseAnswerDiv = document.querySelector("#ai-response")!;
+const aiResponseDiv = document.querySelector("#aiResponseDiv");
+const numberSecondsSpan = document.querySelector("#numberSeconds");
+
 function sendRequest() {
 	const question = (
 		document.querySelector("#questionInput") as HTMLInputElement
 	).value;
+	aiResponseAnswerDiv.innerHTML = "Retrieving answer, please wait...";
+	aiResponseDiv?.classList.remove("block");
+	aiResponseDiv?.classList.add("hidden");
+	totalSeconds = 0;
+	setInterval(setCounter, 1000);
 
 	fetch("http://localhost:8000/send-question", {
 		method: "POST",
@@ -17,7 +49,10 @@ function sendRequest() {
 			return response.text();
 		})
 		.then(data => {
-			console.log(data);
+			aiResponseAnswerDiv!.innerHTML = markdownToHTML(data);
+			numberSecondsSpan!.textContent = totalSeconds.toString();
+			aiResponseDiv?.classList.remove("hidden");
+			aiResponseDiv?.classList.add("block");
 		})
 		.catch(error => {
 			console.error("Error sending request:", error);
