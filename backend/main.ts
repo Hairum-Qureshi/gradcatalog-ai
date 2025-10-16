f08288ebea89750ed2e8c0ebc7fdb82344abeea4
Hairum Qureshi <hqureshi@udel.edu>
2025-10-16 16:25:10 -0400

    replaced text so that if the user asks a question off-topic, it'll send back a message letting them know the AI is unable to answer the question

--- backend/main.ts
+++ backend/main.ts
@@ -2,9 +2,7 @@ import express, { Request, Response } from "express";
 import axios from "axios";
 import { load } from "cheerio";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import readline from "readline"; // only for testing
 import { getPrompt1, getPrompt2 } from "./prompts";
-import fs from "fs"; // only for testing
 import chalk from "chalk"; // only for testing
 import dotenv from "dotenv";
 import path from "path";
@@ -21,11 +19,6 @@ const PORT = 8000;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
 
 export interface CatalogLinkData {
 	text: string;
@@ -54,45 +47,6 @@ async function askAI(
 	gradCISCatalogLinks: CatalogLinkData[],
 	question?: string
 ) {
-	// rl.question(
-	// 	"Please enter a question regarding the UD CIS grad program: ",
-	// 	async question => {
-	// 		const prompt = getPrompt1(question, gradCISCatalogLinks);
-	// 		const result = await model.generateContent(prompt);
-	// 		const response = await result.response;
-	// 		const text = response.text();
-
-	// 		if (text !== "N/A") {
-	// 			const hrefList = text.split(",");
-	// 			console.log(chalk.yellow(hrefList));
-	// 			const pageTexts: PageData[] | PageData = await Promise.all(
-	// 				hrefList.map(getPageContent)
-	// 			);
-	// 			const prompt = getPrompt2(question, hrefList, pageTexts);
-
-	// 			// Handles live AI text generation in terminal via content streaming
-	// 			const result = await model.generateContentStream(prompt);
-	// 			for await (const chunk of result.stream) {
-	// 				const text = chunk.text();
-	// 				if (text) {
-	// 					process.stdout.write(chalk.greenBright(text));
-
-	// 					// For better readability, the AI's response is sent to an 'answers.txt' file that gets created
-	// 					const result = await model.generateContent(prompt);
-	// 					const response = await result.response;
-	// 					const answer = response.text();
-	// 					fs.writeFile("answer.txt", answer, error => {
-	// 						if (error) {
-	// 							console.error(chalk.redBright(error));
-	// 						}
-	// 					});
-	// 				}
-	// 			}
-	// 		} else {
-	// 			console.log("Could not find any applicable reference links for query");
-	// 		}
-	// 	}
-	// );
 	if (question) {
 		const prompt = getPrompt1(question, gradCISCatalogLinks);
 		const result = await model.generateContent(prompt);
@@ -129,7 +83,9 @@ async function askAI(
 			// 	}
 			// }
 		} else {
-			console.log("Could not find any applicable reference links for query");
+			console.log(
+				"Please keep your questions focused on the UD CIS graduate program."
+			);
 		}
 	}
 }
